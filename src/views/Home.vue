d8ba965fe51eef0b97f7aadde0583211eef229eb
huangchao <chao921125@sina.com>
2021-01-21 14:25:19 +0800

    demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,32 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
         <el-button @click="$router.push({ path: '/404' })">to 404</el-button>
         <el-button @click="$router.push({ path: '/error' })">to error</el-button>
+
+        <el-button @click="show = !show">click me</el-button>
+        <transition name="el-zoom-in-center">
+            <div v-show="show" class="transition-box">.el-zoom-in-center</div>
+        </transition>
+
+        <transition name="el-zoom-in-top">
+            <div v-show="show" class="transition-box">.el-zoom-in-top</div>
+        </transition>
+
+        <transition name="el-zoom-in-bottom">
+            <div v-show="show" class="transition-box">.el-zoom-in-bottom</div>
+        </transition>
+        <transition name="el-zoom-in-left">
+            <div v-show="show" class="transition-box">.el-zoom-in-left</div>
+        </transition>
+        <transition name="el-zoom-in-right">
+            <div v-show="show" class="transition-box">.el-zoom-in-right</div>
+        </transition>
+        <el-collapse-transition>
+            <div v-show="show">
+                <div class="transition-box">el-collapse-transition</div>
+                <div class="transition-box">el-collapse-transition</div>
+            </div>
+        </el-collapse-transition>
     </div>
 </template>
 
@@ -13,6 +37,7 @@ export default {
         return {
             imgSrc: [require("@/assets/logo.png"), require("@/assets/logo.png")],
             isCollapse: false,
+            show: false,
         };
     },
     methods: {
@@ -23,4 +48,12 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.transition-box {
+    margin-right: 20px;
+    margin-bottom: 20px;
+    width: 200px;
+    height: 100px;
+    background-color: aqua;
+}
+</style>
